e0601700422453e8da56eb167f924b5ad15b47d6
Patrick Gomes <66369808+Patotricks15@users.noreply.github.com>
2023-01-29 07:28:26 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import openai
+import json
+import re
+
+
+class GeradorMonstro:
+    def __init__(self):
+        openai.api_key = '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'
+
+    def montar_prompt(self, genero_input, nome_input, raca_input, tamanho_input, alinhamento_input):
+        self.prompt = f"""Gere em pt-br a descrição longa de um monstro de RPG de mesa com as seguintes informações:
+                    Name: {nome_input}
+                    Race: {raca_input}
+                    Size: {tamanho_input}
+                    Alignment: {alinhamento_input}
+                    Gender: {genero_input}"""
+    def gerar_descricao(self):
+        response=openai.Completion.create(
+                                            model='text-davinci-003',
+                                            prompt= self.prompt,
+                                            max_tokens=2000
+                    )
+        resposta_raw = json.loads(str(response))['choices'][0]['text']
+        resposta = max(re.findall("(.+?)(?:\n|\r|$)", resposta_raw), key=len) #Pegando o maior parágrafo, consertando o erro de completar respostas
+        self.descricao = resposta.strip()
+        return self.descricao
+    def gerar_imagem(self):
+        tldr = '\n\nTl;dr'
+        response = openai.Image.create(
+                                    prompt=f"""Monstro de RPG de mesa estilo portrait 2d medieval rpg anime black and white art.
+                                    Aparência: {self.descricao+tldr}""",
+                                    n=1,
+                                    size="256x256"
+                                    )
+        image_url = response['data'][0]['url']
+        self.imagem = f"""![Foo]({image_url})"""
+        return self.imagem
+    def gerar_resposta(self):
+        self.gerar_descricao()
+        self.gerar_imagem()
+        return self.imagem + self.descricao
---
+++ mvp.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from main import *
+
+lista_racas = ['beast', 'dragon', 'humanoid', 'monstrosity', 'fiend', 'undead',
+       'elemental', 'giant', 'swarm of tiny beasts', 'construct', 'plant',
+       'celestial', 'fey', 'aberration', 'ooze', 'Fiend']
+
+lista_tamanho = ['Medium', 'Large', 'Huge', 'Tiny', 'Small', 'Gargantuan']
+
+lista_alinhamento = ['Unaligned', 'Chaotic Evil', 'Lawful Evil', 'Neutral Evil',
+       'Lawful Good', 'Neutral', 'Any Alignment', 'Chaotic Good', 'Neutral Good', 'Any Non-good Alignment',
+       'Chaotic Neutral', 'Lawful Neutral', 'Any Non-lawful Alignment',
+       'Any Chaotic Alignment', 'Any',
+       'Neutral Good (50%) Or Neutral Evil (50%)', 'Any Evil Alignment']
+lista_genero = ['male','female','Undefined']
+
+
+st.title('Gerador de monstros de RPG')
+
+nome_select = st.text_input('Nome')
+raca_select = st.selectbox('Raças', lista_racas)
+tamanho_select = st.selectbox('Tamanhos', lista_tamanho)
+alinhamento_select = st.selectbox('Alinhamento', lista_alinhamento)
+genero_select = st.selectbox('Genero', lista_genero)
+gerador = GeradorMonstro()
+
+if st.button('Gerar monstro'):
+    gerador.montar_prompt(nome_input = nome_select,
+                        raca_input = raca_select,
+                        tamanho_input = tamanho_select,
+                        alinhamento_input = alinhamento_select,
+                        genero_input = genero_select)
+
+    st.markdown(gerador.gerar_descricao())
+    st.markdown(gerador.gerar_imagem())
+    #st.markdown(gerador.gerar_imagem())
